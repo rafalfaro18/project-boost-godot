dcd018685b6cdc505d38f2f57627f1c5cc8e1988
Rafalfaro <16846380+rafalfaro18@users.noreply.github.com>
2025-10-01 22:00:03 -0600

    Print elapsed time

--- player.gd
+++ player.gd
@@ -1,14 +1,15 @@
 extends Node3D
 
+var timer:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello World")
-	print("Don't Panic")
-	print(42)
-
+	var my_number: int = 10
+	print(my_number + 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		print("space bar was pressed!")
+	timer = timer + delta
+	print(timer)
