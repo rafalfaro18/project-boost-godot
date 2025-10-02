0e683df7eb5b8672ab33bca6d9ff4ac80fe31436
Rafalfaro <16846380+rafalfaro18@users.noreply.github.com>
2025-10-01 21:53:38 -0600

    Log only the first time space bar is pressed

--- player.gd
+++ player.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
 		print("space bar was pressed!")
